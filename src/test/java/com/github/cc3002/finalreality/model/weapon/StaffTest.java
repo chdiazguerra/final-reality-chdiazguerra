7ce142e9518ac097f13aef3f9f4e1a84a8381e4c
Christian Diaz <christian.diaz.g@ug.uchile.cl>
2020-10-14 00:45:56 -0300

    Tarea-1

--- src/test/java/com/github/cc3002/finalreality/model/weapon/StaffTest.java
+++ src/test/java/com/github/cc3002/finalreality/model/weapon/StaffTest.java
@@ -1,10 +1,15 @@
 package com.github.cc3002.finalreality.model.weapon;
 
-import com.github.chdiazguerra.finalreality.model.weapon.Axe;
 import com.github.chdiazguerra.finalreality.model.weapon.Staff;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * A class containing the test for Staff type weapon.
+ *
+ * @author Christian Díaz Guerra
+ * @see Staff
+ */
 class StaffTest extends AbstractWeaponTest{
     @BeforeEach
     void setUp(){
